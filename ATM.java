247e5c539826c51c20d32d6190053af1a59d41bd
arpitr18 <114918704+arpitr18@users.noreply.github.com>
2024-04-09 19:11:36 +0530

    Updated ATM.java
    List of updated Items are : 
    => Added Thread,sleep
    => verify the pin , you have only 3 chances

--- ATM.java
+++ ATM.java
@@ -1,97 +1,97 @@
 package ATMproject;
 
 import java.util.*;
+
 public class ATM {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
-        System.out.print("-----------------------------Welcome to ATM-----------------------------");
-        int balance = 10000;
+        System.out.println("-----------------------------Welcome to ATM-----------------------------");
+
+        int balance = 3000;
         System.out.println("Please Insert your Debit Card ");
-        System.out.print("PLease Enter your PIN: ");
-        int pin = sc.nextInt();
-          while(true){
-               if (pin==1234) {
-                   System.out.println("------------------------Welcome of SBI ACCOUNT-----------------------");
-                   System.out.println("Press 1 to View Your Balance");
-                   System.out.println("Press 2 to Make a Withdrawal");
-                   System.out.println("Press 3 to Deposit Money");
-                   System.out.println("Press 4 to Exit");
-                   System.out.print("What would you like to choose: ");
-                   int option = sc.nextInt();
-                   if(option == 1){
-                       System.out.println("Your Balance is " + balance);
-                       System.out.print("Would you like to continue?(y/n)");
-                       char loop = sc.next().charAt(0);
-                       if (loop == 'n'){
-                           System.out.println("-----------Please wait while your card is returned----------");
-                           System.out.println("---------------Thank you for visiting us--------------------");
-                           break;}
-                   } else if (option == 2) {
-                       System.out.print("Enter the money to be withdrawn");
-                       int withdraw = sc.nextInt();
-                       if (balance >= withdraw) {
-                           balance -= withdraw;
-                           System.out.println("Your transaction is successful.");
-                           System.out.println("Your current balance is " + balance);
-                           System.out.println("Please collect your Cash.");
-                       }else {
-                           System.out.println("Insufficient balance");
-                       }
-                       System.out.print("Would you like to continue?(y/n)");
-                       char loop = sc.next().charAt(0);
-//
-                       if(loop == 'n') {
-                           System.out.println("-----------Please wait while your card is returned----------");
-                           System.out.println("---------------Thank you for visiting us--------------------");
-                           break;}
+        Thread.sleep(5000);
+        int chances = 3;
+        while (chances != 0) {
 
+            System.out.print("PLease Enter your PIN: ");
+            int pin = sc.nextInt();
+            System.out.println("Please wait, we are verifying you PIN......");
+            Thread.sleep(1000);
+            if (pin == 1234) {
+                System.out.println("---------------------------LOGIN SUCCESSFUL--------------------------");
 
-                   } else if (option == 3) {
-                       System.out.println("Enter the amount you would like to Deposit: ");
-                       int deposit = sc.nextInt();
-                       balance += deposit;
-                       System.out.println("Your transaction is successful.");
-                       System.out.println("Your current balance is " + balance);
+                Thread.sleep(1000);
+                System.out.println("------------------------Welcome of SBI ACCOUNT-----------------------");
+                System.out.println("Press 1 to View Your Balance");
+                System.out.println("Press 2 to Make a Withdrawal");
+                System.out.println("Press 3 to Deposit Money");
+                System.out.println("Press 4 to Exit");
+                System.out.print("What would you like to choose: ");
+                int option = sc.nextInt();
+                if (option == 1) {
+                    System.out.println("Your Balance is " + balance);
+                    System.out.print("Would you like to continue?(y/n)");
+                    char loop = sc.next().charAt(0);
+                    if (loop == 'n') {
+                        System.out.println("-----------Please wait while your card is returned----------");
+                        Thread.sleep(3000);
+                        System.out.println("---------------Thank you for visiting us--------------------");
+                        break;
+                    }
+                } else if (option == 2) {
+                    System.out.print("Enter the money to be withdrawn");
+                    int withdraw = sc.nextInt();
+                    if (balance >= withdraw) {
+                        balance -= withdraw;
+                        System.out.println("Your transaction is successful.");
+                        System.out.println("Your current balance is " + balance);
+                        System.out.println("Please collect your Cash.");
+                    } else {
+                        System.out.println("Insufficient balance");
+                    }
+                    System.out.print("Would you like to continue?(y/n)");
+                    char loop = sc.next().charAt(0);
+                    //
+                    if (loop == 'n') {
+                        System.out.println("-----------Please wait while your card is returned----------");
+                        Thread.sleep(3000);
+                        System.out.println("---------------Thank you for visiting us--------------------");
+                        break;
+                    }
 
-                       System.out.print("Would you like to continue?(y/n)");
-                       char loop = sc.next().charAt(0);
+                } else if (option == 3) {
+                    System.out.println("Enter the amount you would like to Deposit: ");
+                    int deposit = sc.nextInt();
+                    balance += deposit;
+                    System.out.println("Your transaction is successful.");
+                    System.out.println("Your current balance is " + balance);
 
-                       if(loop == 'n') {
-                           System.out.println("-----------Please wait while your card is returned----------");
-                           System.out.println("---------------Thank you for visiting us--------------------");
-                           break;}
+                    System.out.print("Would you like to continue?(y/n)");
+                    char loop = sc.next().charAt(0);
 
-                   } else if (option == 4) {
-                       System.out.println("-----------Please wait while your card is returned----------");
-                       System.out.println("---------------Thank you for visiting us--------------------");
-                       break;
-                   }
-                   else{
-                       System.out.println("You select a valid option");
-                   }
-               } else{
-                   System.out.println("Incorrect Password.");
-                   System.out.println("Please try again.");
-                   break;
+                    if (loop == 'n') {
+                        System.out.println("-----------Please wait while your card is returned----------");
+                        Thread.sleep(3000);
+                        System.out.println("---------------Thank you for visiting us--------------------");
+                        break;
+                    }
 
+                } else if (option == 4) {
+                    System.out.println("-----------Please wait while your card is returned----------");
+                    Thread.sleep(3000);
+                    System.out.println("---------------Thank you for visiting us--------------------");
+                    break;
+                } else {
+                    System.out.println("You select a valid option");
+                }
+            } else {
+                System.out.println("---------------------------LOGIN FAILED------------------------------");
+                System.out.println("Incorrect pin.");
+                chances = chances - 1;
+                System.out.println("Please try again. You have " + chances + " chances left.");
 
+            }
 
-               }
-
-          }
-    }}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+    }
+}
